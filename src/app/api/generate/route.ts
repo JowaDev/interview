984d687940ee4bb7411f297bb635709ba4ba0eec
Joiakim Dasek <jowadev@gmail.com>
2024-04-26 14:22:36 +0200

    I added openai dependency and useChat

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,4 +1,34 @@
-export const dynamic = 'force-dynamic'
+import OpenAI from 'openai';
+import {OpenAIStream, StreamingTextResponse} from 'ai';
 
-export async function POST(request: Request) {
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const dynamic = 'force-dynamic';
+
+export const edge = true;
+
+export async function POST(req: Request) {
+    try {
+        const {messages} = await req.json();
+
+        const response = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo-0125',
+            stream: true,
+            response_format: {type: 'json_object'},
+            messages,
+        });
+
+        const stream = OpenAIStream(response);
+
+        return new StreamingTextResponse(stream);
+    } catch (error) {
+        if (error instanceof OpenAI.APIError) {
+            const {name, status, headers, message} = error;
+            return Response.json({name, status, headers, message}, {status});
+        } else {
+            throw error;
+        }
+    }
 }
