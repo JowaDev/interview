248af2b6c21aa085082749bd4e44b7f3c77a4971
zotrim <zotrim.uka98@hotmail.fr>
2024-04-26 14:30:13 +0200

    dropdown page reanme to dropzone

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -6,17 +6,17 @@ import {CarouselNext, CarouselPrevious} from "@/components/ui/carousel";
 import {Input} from "@/components/ui/input"
 import {Label} from "@/components/ui/label"
 
-interface Dropdown {
+interface Dropzone {
 
 }
 
-export const Dropdown: FC<Dropdown> = () => {
+export const Dropzone: FC<Dropzone> = () => {
     return (
         <div className='mt-36 ml-16'>
-            <h1 className="text-4xl mt-6">Déposer vos fichiers</h1>
+            <h1 className="text-4xl mt-6">Déposez vos fichiers</h1>
             <div className='mt-16 flex flex-col gap-6 justify-center'>
                     <Label htmlFor="picture" >Drop zone</Label>
-                    <Input id="picture" type="file" className="border-dashed border-black h-36" />
+                    <Input id="picture" type="file" accept='' className="border-dashed border-black h-36"/>
             </div>
             <div className='mt-36 w-full gap-4 flex justify-between'>
                 <CarouselPrevious
