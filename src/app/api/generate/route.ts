5ee2fee946282ed16fbe15f6a5e84beb9915997c
Joiakim Dasek <jowadev@gmail.com>
2024-05-25 14:52:15 +0200

    Changed max delay for serverless functions to 30s fixed

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -4,8 +4,7 @@ import OpenAI from "openai";
 import {dropZonePrompt, skillsPrompt} from "@/lib/prompts";
 
 export const dynamic = 'force-dynamic';
-
-export const maxDuration = 60
+export const maxDuration = 30;
 
 export async function POST(request: Request) {
     const {summarizeContent, interviewSelectionTypeState, jobSelection, skills}: {
