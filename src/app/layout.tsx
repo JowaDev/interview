b040e29a30afd6a299724072cfe27c63ab379dc2
Joiakim Dasek <jowadev@gmail.com>
2024-05-29 20:23:53 +0200

    Layout to tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import type {Metadata} from "next";
 import {Inter as FontSans} from "next/font/google"
 import "./globals.css";
 import {cn} from "@/lib/utils";
+import {ReactNode} from "react";
 import {GlobalContext} from "@/components/GlobalContext";
 
 const fontSans = FontSans({
@@ -8,14 +10,16 @@ const fontSans = FontSans({
     variable: "--font-sans",
 })
 
-export const metadata = {
+export const metadata: Metadata = {
     title: "Jobs Interview",
     description: "This Web Application will help you to prepare for your next job interview",
 };
 
 export default function RootLayout({
                                        children,
-                                   }) {
+                                   }: Readonly<{
+    children: ReactNode;
+}>) {
     return (
         <html lang="fr" suppressHydrationWarning>
         <body className={cn(
